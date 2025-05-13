8921a1ef398674137bda4642d03cdd83ae4eb625
Filip Dimovski <filip.dimovskifico@gmail.com>
2025-05-13 02:52:45 +0200

    The start of something new!
    Valkron Engine init and cmake dll test

---
+++ Sandbox/src/app.cpp
@@ -0,0 +1,9 @@
+
+namespace Valkron{
+    __declspec(dllimport) void Print();
+}
+
+int main(){
+    Valkron::Print();
+    return 0;
+}
---
+++ Valkron/src/test.cpp
@@ -0,0 +1,9 @@
+#include "test.hpp"
+
+#include <iostream>
+
+namespace Valkron{
+    void Print(){
+        std::cout << "Welcome to Valkron Engine!" << std::endl;
+    }
+}
